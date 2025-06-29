0ceb22962f7cbf1fd494b6bf08e81e5da50ac4c3
Majid Qurashi <106837825+majid-qurashi@users.noreply.github.com>
2025-06-29 16:46:41 +0530

    Add content.ts

---
+++ src/data/content.ts
@@ -0,0 +1,33 @@
+export default [
+  {
+    title: 'Tutorial_1 for the Python programming course scheduled',
+    description: 'on April 21, 2025, Venue-CS1, Timing: 10:15am',
+    url: 'https://classroom.google.com/w/NzUzMDI2NDMxNjk0/t/all',
+    image: 'assets/poetry.png',
+    publised_date: '2025-04-20',
+  },
+  {
+    title: 'GCET 7th Industrial Training 2025',
+    description:
+      'CSE 7th Semester Industrial Training for the information of the students',
+    url: 'https://docs.google.com/forms/d/1AuDGiHXPtv5EFwZt1KRVwQyR4CznEc7aAm9vnxdJwS0/preview',
+    image: 'assets/poetry.png',
+    publised_date: '2025-04-15',
+  },
+  {
+    title: 'GCET 5th Semester SE/PP Topics',
+    description:
+      'Presentation topics for the 5th Semester Software Engineering and Python programming',
+    url: 'https://docs.google.com/forms/d/1-H5io1u_zKjh-QINOnYm_tOycRrtsrhWZtb1Wgl6EG4/preview',
+    image: 'assets/poetry.png',
+    publised_date: '2025-05-26',
+  },
+  {
+    title: 'GCET 5th Sem CSE Syllabus 2025',
+    description:
+      'Updated syllabus for the 2025 academic year for the CSE department',
+    url: 'https://mushtaqdarblog.wordpress.com/wp-content/uploads/2025/04/cse-2022-onwards-syllabus-_mushtaq.pdf',
+    image: 'assets/poetry.png',
+    publised_date: '2025-05-10',
+  },
+];
